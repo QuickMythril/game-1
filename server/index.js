947347ab246e9d634d4a1e3a7597543ecaca3e26
PhilReact <philliplangmartinez@gmail.com>
2024-04-27 13:47:47 +0300

    added root package.json

--- server/index.js
+++ server/index.js
@@ -10,6 +10,17 @@ app.use(cors());
 
 const server = http.createServer(app);
 
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    //Set static folder
+    app.use(express.static("client/build"));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
 const connectedUsers = new Map();
 const games = {}; // Key: roomId, Value: GameState instance
 
@@ -216,6 +227,7 @@ io.on("connection", (socket) => {
    }
   });
 });
+
 
 server.listen(3001, () => {
   console.log("server is running");
